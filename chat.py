e0a5a159824865753c3a5c4f2429245ee3f55024
Deepika-SenthilKumar <deepisk1994@gmail.com>
2026-01-17 17:55:17 +0530

    Add files via upload

--- chat.py
+++ chat.py
@@ -20,7 +20,7 @@ st.set_page_config(page_title="AI Echo — Sentiment Dashboard", layout="wide")
 model_path = r"D:\AiEcho\NLP_model.pkl"
 
 try:
-    NLP_model = pickle.load(open("nlp_model.pkl", "rb"))
+    nlp_model = pickle.load(open("nlp_model.pkl", "rb"))
     tfidf = pickle.load(open("tfidf_vectorizer.pkl", "rb"))
 
 
